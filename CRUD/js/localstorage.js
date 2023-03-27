f5b8f08bf6b88b56735b1030e5713640733be9e4
Saul P <saulpulido52@gmail.com>
2023-03-26 22:11:38 -0700

    se perdio el editar...

--- CRUD/js/localstorage.js
+++ CRUD/js/localstorage.js
@@ -1,40 +1,33 @@
 const nombre = document.getElementById("nombreInput");
 const apellido = document.getElementById("apellidoInput");
-const usuarios = JSON.parse(localStorage.getItem("usuarios")  ) || [];
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 const cuerpoTabla = document.getElementById("cuerpoTabla");
 const editandoUsuario = document.getElementById("editandoUsuario");
 const btnEdit = document.getElementById("editando");
 let editorUsuario = null;
 
+const agregarUsuarios = () => {
+  const usuario = {
+    id: crypto.randomUUID(),
+    nombre: nombre.value,
+    apellido: apellido.value,
+  };
 
-console.log(usuarios);
+  usuarios.push(usuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-const agregarUsuarios = () =>{
-    const usuario = {
-        id: crypto.randomUUID(),
-        nombre: nombre.value,
-        apellido : apellido.value
-    }
+  console.log(usuario);
+};
 
-    usuarios.push(usuario);
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
-
-
-    console.log(usuario);
+function mostrarUsuarios() {
+  cuerpoTabla.innerHTML = "";
+  usuarios.forEach((usuario) => {
+    cuerpoTabla.innerHTML += `</tr><th scope="row">${usuario.id}</th><td>${usuario.nombre}</td><td>${usuario.apellido}</td><td><button type="button col" class="btn ${editorUsuario === usuario ? "btn-primary" : "btn-warning"}" onclick="editarUsuario('${usuario.id}')">${editorUsuario === usuario ? "Guardar" : "Editar"}</button><button type="button" class="col btn btn-primary" onclick="eliminarUsuarios('${usuario.id}')">Eliminar</button> </td></tr>`;
+  });
 }
 
-
-
-function mostrarUsuarios(){
-    cuerpoTabla.innerHTML = "";
-    usuarios.forEach(usuario => {
-        cuerpoTabla.innerHTML += `</tr><th scope="row">${usuario.id}</th><td>${usuario.nombre}</td><td>${usuario.apellido}</td><td><button type="button col" class="btn btn-warning" onclick="editarUsuario(nombre,apellido)">Editar</button><button type="button" class="col btn btn-primary" onclick="eliminarUsuarios('${usuario.id}')">Eliminar</button> </td></tr>`
-    });
-    
-}
-function eliminarUsuarios (id) {
-  
-    mostrarUsuarios();
+function eliminarUsuarios(id) {
+  mostrarUsuarios();
 
   for (let i = 0; i < usuarios.length; i++) {
     if (usuarios[i].id === id) {
@@ -43,23 +36,18 @@ function eliminarUsuarios (id) {
     }
   }
 
-  localStorage.setItem('usuarios', JSON.stringify(usuarios));
-
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
 }
-const editarUsuario = (id) => {
-    editandoUsuario.textContent= "Editando Usuario";
-    mostrarUsuarios();
-    for (let i = 0; i < usuarios.length; i++) {
-        if (usuarios[i].id === id) {
-         usuarios.nombre= nombre.value;
-         usuarios.apellido= apellido.value;
-          
-        }
-      }
-    btnEdit.textContent="Editar";
-    btnEdit.classList.add("btn-warning");
-    
 
-    
-  };
-  
+const editarUsuario = (id) => {
+  editandoUsuario.textContent = "Editando Usuario";
+  mostrarUsuarios();
+  editorUsuario = usuarios.find((usuario) => usuario.id === id);
+  if (editorUsuario) {
+    nombre.value = editorUsuario.nombre;
+    apellido.value = editorUsuario.apellido;
+    btnEdit.textContent = "Guardar";
+    btnEdit.classList.remove("btn-warning");
+    btnEdit.classList.add("btn-primary");
+  }
+};
