820a5f17ff68d9bf67cf4c3e42456541603f7983
Saul P <saulpulido52@gmail.com>
2023-03-29 18:36:38 -0700

    termineeeee el guardado muchas gracias

--- CRUD/js/localstorage.js
+++ CRUD/js/localstorage.js
@@ -5,24 +5,35 @@ const cuerpoTabla = document.getElementById("cuerpoTabla");
 const editandoUsuario = document.getElementById("editandoUsuario");
 const btnEdit = document.getElementById("editando");
 let editorUsuario = null;
+let editando = false;
+let idEditar ='';
 
 const agregarUsuarios = () => {
-  const usuario = {
-    id: crypto.randomUUID(),
-    nombre: nombre.value,
-    apellido: apellido.value,
+  if (!editando){
+    const usuario = {
+      id: crypto.randomUUID(),
+      nombre: nombre.value,
+      apellido: apellido.value,
+    };
+  
+    usuarios.push(usuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  
+    console.log(usuario);
+
+  }
+  else {
+    aceparEditarUsuario();
+
   };
-
-  usuarios.push(usuario);
-  localStorage.setItem("usuarios", JSON.stringify(usuarios));
-
-  console.log(usuario);
+  
 };
 
 function mostrarUsuarios() {
   cuerpoTabla.innerHTML = "";
   usuarios.forEach((usuario) => {
-    cuerpoTabla.innerHTML += `</tr><th scope="row">${usuario.id}</th><td>${usuario.nombre}</td><td>${usuario.apellido}</td><td><button type="button col" class="btn ${editorUsuario === usuario ? "btn-primary" : "btn-warning"}" onclick="editarUsuario('${usuario.id}')">${editorUsuario === usuario ? "Guardar" : "Editar"}</button><button type="button" class="col btn btn-primary" onclick="eliminarUsuarios('${usuario.id}')">Eliminar</button> </td></tr>`;
+    cuerpoTabla.innerHTML += `</tr><th scope="row">${usuario.id}</th><td>${usuario.nombre}</td>
+    <td>${usuario.apellido}</td><td><button type="button col" class="btn ${editorUsuario === usuario ? "btn-primary" : "btn-warning"}" onclick="editarUsuario('${usuario.id}')">${editorUsuario === usuario ? "Guardar" : "Editar"}</button><button type="button" class="col btn btn-primary" onclick="eliminarUsuarios('${usuario.id}')">Eliminar</button> </td></tr>`;
   });
 }
 
@@ -43,6 +54,8 @@ const editarUsuario = (id) => {
   editandoUsuario.textContent = "Editando Usuario";
   mostrarUsuarios();
   editorUsuario = usuarios.find((usuario) => usuario.id === id);
+  editando=true;
+  idEditar = id;
   if (editorUsuario) {
     nombre.value = editorUsuario.nombre;
     apellido.value = editorUsuario.apellido;
@@ -50,4 +63,20 @@ const editarUsuario = (id) => {
     btnEdit.classList.remove("btn-warning");
     btnEdit.classList.add("btn-primary");
   }
+};
+
+function aceparEditarUsuario(){
+
+  editorUsuario = usuarios.findIndex((usuario) => usuario.id === idEditar);
+  usuarios[editorUsuario].nombre = nombre.value;
+  usuarios[editorUsuario].apellido = apellido.value;
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  editando=false;
+  idEditar='';
+  nombre.value='';
+  apellido.value='';
+  editandoUsuario.textContent = "Registro de Usuarios"
+
+
+
 };
